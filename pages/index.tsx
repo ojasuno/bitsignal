413d8efcede2d8bdbe2bab4de43a8810f17b435b
ojasuno <ojasuno21@gmail.com>
2023-03-17 16:48:38 +1100

    public

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,14 +30,14 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   return (
     <>
       <Head>
-        <title>Next.js Conf 2022 Photos</title>
+        <title>BitSignal One</title>
         <meta
           property="og:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
+          content="https://www.bitsignal.one/og-image.png"
         />
         <meta
           name="twitter:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
+          content="https://www.bitsignal.one/og-image.png"
         />
       </Head>
       <main className="mx-auto max-w-[1960px] p-4">
