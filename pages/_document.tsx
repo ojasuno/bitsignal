9846fa096c0462f3fdf624cd2d5e5b2fe27a9751
ojasuno <ojasuno21@gmail.com>
2023-03-17 16:43:28 +1100

    public

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,20 +7,20 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta
-            name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            name="BitSignal One"
+            content="A website for The BitSignal"
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="https://www.bitsignal.one/" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="A website for The BitSignal."
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="BitSignal One" />
+          <meta name="twitter:card" content="BitSignal One" />
+          <meta name="twitter:title" content="BitSignal One" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="A website for The BitSignal"
           />
         </Head>
         <body className="bg-black antialiased">
